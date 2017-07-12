567629b1d30859abda02ae44400a03f5f037702d
Sally Bradfield <salzeo@gmail.com>
2017-07-12 15:29:26 -0600

    folder creation

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<	UJSCpaRu3QU6Idn69FmLtN3fB >',
+  consumer_secret: '<	t7UoXmPvsWQ4cA9SSLRSo4BlovGUTEh2saiYzEWbQMpWBZ6FPE >',
+  access_token_key: '<	884884796538748928-SmzhPWPCAHE2pbI05TQ8j8sOJ5XGxdG >',
+  access_token_secret: '<bH2tT9FCsE8UTopI2WrMC0aJ7H6wRrZ2ulmfGemZZHDGL >',
+}
